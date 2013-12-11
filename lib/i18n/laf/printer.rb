58c10bb48d013f0c5560b750e07516e7cae10037
pinzolo <pinzolo@gmail.com>
2013-12-11 17:31:06 +0900

    Modify printer.rb * output locales * output home directory

--- lib/i18n/laf/printer.rb
+++ lib/i18n/laf/printer.rb
@@ -6,8 +6,28 @@ module I18n::LaF
     end
 
     def print
-      if @lost_items.is_a?(Array) && !@lost_items.empty?
-        puts @lost_items.map(&:to_s).join("\n")
+      print_home
+      puts
+      print_locales
+      puts
+      print_lost_items
+    end
+
+    private
+    def print_home
+      puts "Home directory: #{@office.home}"
+    end
+
+    def print_locales
+      tag = @office.dictionaries.size == 1 ? "Locale" : "Locales"
+      puts "#{tag}: #{@office.dictionaries.map(&:locale).join(", ")}"
+    end
+
+    def print_lost_items
+      if @office.lost_items.is_a?(Array) && !@office.lost_items.empty?
+        puts "Lost items:"
+        puts "============"
+        puts @office.lost_items.map(&:to_s).join("\n")
       else
         puts "There is no lost item, Yeah!"
       end
