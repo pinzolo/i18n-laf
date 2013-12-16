dfdd6cb869cd816c1ef39191cfae9b4e5607cea0
pinzolo <pinzolo@gmail.com>
2013-12-16 22:19:24 +0900

    Refactoring

--- lib/i18n/laf/dictionary.rb
+++ lib/i18n/laf/dictionary.rb
@@ -1,19 +1,13 @@
 # coding: utf-8
 module I18n::LaF
   class Dictionary
-    attr_reader :locale, :leaves
+    attr_reader :locale, :leaves, :keys, :data
 
     def initialize(leaf, base = nil)
       @locale = leaf.locale
       @leaves = base ? base.leaves + [leaf] : [leaf]
-    end
-
-    def keys
-      @keys ||= @leaves.inject([]) { |keys, leaf| keys + leaf.keys }
-    end
-
-    def data
-      @data ||= @leaves.inject({}) { |data, leaf| data.merge(leaf.data) }
+      @keys = @leaves.inject([]) { |keys, leaf| keys + leaf.keys }
+      @data = @leaves.inject({}) { |data, leaf| data.merge(leaf.data) }
     end
   end
 end
